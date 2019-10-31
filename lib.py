925ec25b156747aa60c6656ef851c1ce3965ccb0
Tie Bridgwood <tiebridgwood@Ties-MacBook-Pro.local>
2019-10-31 16:19:20 +0000

    changes made thursday

--- lib.py
+++ lib.py
@@ -3,5 +3,26 @@
 class MongoHandler():
     pass
 
-class WeatherGetter():
-    pass
+class Weather():
+    def __init__(self):
+        self.url = 'https://api.darksky.net/forecast/'
+        self.key = os.getenv("DARKSKYKEY")
+        self.weather_days = []
+        self.rains = []
+    
+    def getweather(latitude, longitude, date_list):
+        for date in date_list:
+            response = requests.get(f'https://api.darksky.net/forecast/{self.key}/{latitude},{[longitude]},{date}T15:00:00?exclude=minutely,hourly,daily,alerts,flags')
+            r = response.json()
+            self.weather_days.append(r)
+            
+    def rainmaker(self):
+        for weather_day in self.weather_days:
+            if 'rain' in weather_day['currently']['icon']:
+                self.rains.append(True)
+            else:
+                self.rains.append(False)
+
+def make_df(table_name): # Takes the name of a table and makes a pandas dataframe from it
+    return pd.read_sql_query(f"""SELECT * FROM {table_name}""", con)
+        
